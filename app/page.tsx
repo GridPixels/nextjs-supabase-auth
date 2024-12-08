1c90fc5b454ab2e2b3ca9b57242f40a1566df82e
bushra <bushracodes@gmail.com>
2024-12-08 17:05:11 +0600

    updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen gap-4">
-     
-     
       <Button asChild size={"lg"}>
         <Link href="/login">Login</Link>
       </Button>
